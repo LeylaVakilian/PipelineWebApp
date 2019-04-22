e131cb5f276dc66992d3fd67ac6f06a08a192d55
imanthonny <34114220+imanthonny@users.noreply.github.com>
2019-04-22 09:45:56 -0400

    Add files via upload

--- DropdownPage/routes/index.js
+++ DropdownPage/routes/index.js
@@ -6,13 +6,23 @@ const shell = require('shelljs');
 var fs = require("fs");
 var app = express();
 
+var connection = mysql.createConnection({
+  host: "pachydermdatabase.mysql.database.azure.com",
+  user: "anthony@pachydermdatabase",
+  password: "Bhklab1234@",
+  database : 'pachyderm'
+});
+
+
+
+
 app.use(express.static('public'))
-
-
 
 router.get('/', function(req, res, next) {
   	res.render('index');
 });
+
+
 
 router.post('/execute', function (req, res, next) {
 	const osr = req.body.osr;
@@ -24,8 +34,40 @@ router.post('/execute', function (req, res, next) {
     const bvr = req.body.bvr;
     const email = req.body.email;
 
-    fs.writeFile("/Users/anthonymammoliti/Desktop/PSetCreation/selection.txt", email + '\n' + osr + '\n' + genomer + '\n' + g2r + '\n' + verr + '\n' + corr + '\n' + phr2 + '\n' + bvr , function (err) {
-    shell.exec('/Users/anthonymammoliti/Desktop/PSetCreation/process.sh');
+
+connection.connect(function(err) {
+  if (err) throw err;
+  var sql = "SELECT DOI FROM test WHERE dataset = ('" + osr + "') AND genome = ('" + genomer + "')";
+  // var sql = "INSERT INTO test (dataset, tester) VALUES ('" + osr + "', '" + genomer + "')";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+
+
+
+    if (result === undefined || result.length == 0) {
+
+   console.log("empty" + result);
+   var sql2 = "INSERT INTO test(`dataset`, `genome`) VALUES ('" + osr + "', '" + genomer + "')";
+
+   connection.query(sql2, function (err, result) {
+   if (err) throw err;
+   console.log(result);
+
+
+
+      });
+
+}
+  });
+
+  connection.end();
+});
+
+
+
+    fs.writeFile("/Users/anthonymammoliti/Desktop/selection.txt", email + '\n' + osr + '\n' + genomer + '\n' + g2r + '\n' + verr + '\n' + corr + '\n' + phr2 + '\n' + bvr , function (err) {
+    shell.exec('~/Desktop/anthonymammoliti/Desktop/process.sh');
     // Checks if there is an error
     if (err) return console.log(err);
     });
